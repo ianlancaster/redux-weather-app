b3bed9ab9f9f20c3ce4a763aa43f1177139f888a
Ian Lancaster <ianclancaster@gmail.com>
2016-11-16 22:28:04 -0700

    creates main.js

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+// link to combined reducer
+// link to main application container
+
+const middleware = [ thunk ]
+
+const store = createStore(
+//  reducer,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
+    {/* <App /> */}
+  </Provider>,
+  document.getElementById('root')
+)
