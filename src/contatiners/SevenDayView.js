343e0e5487ff161633c8ca1d63f983d9cf0163c5
Ian Lancaster <ianclancaster@gmail.com>
2016-11-16 23:53:48 -0700

    populates view containers with container template

---
+++ src/contatiners/SevenDayView.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+class SevenDayView extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired
+  }
+
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SevenDayView)
