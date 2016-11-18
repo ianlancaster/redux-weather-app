798163e029de1a8ba54e9bb2723a3fb34ab80ffd
Ian Lancaster <ianclancaster@gmail.com>
2016-11-18 12:28:28 -0700

    redux state persists to localStorage

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import persistState from 'redux-localstorage'
 import reducer from './reducers'
 import App from './contatiners/App'
 import './styles/core.scss'
+
+const enhancer = compose(
+  /* [middlewares] */
+  persistState(/*paths, config*/),
+)
 
 const middleware = [ thunk ]
 
 const store = createStore(
   reducer,
-  applyMiddleware(...middleware)
+  applyMiddleware(...middleware),
+  enhancer
 )
 
 render(
